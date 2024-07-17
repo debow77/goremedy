0c0f1193300e143defb7793c5ebed36d4b7b8943
debow77 <dscottpowell@gmail.com>
2024-07-17 17:40:53 -0500

    Create test_company.go

---
+++ companies/test_company.go
@@ -0,0 +1,38 @@
+package companies
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Create a new RemedyClient
+	client, err := remedy.NewRemedyClient()
+	if err != nil {
+		log.Fatalf("Error creating Remedy client: %v", err)
+	}
+
+	// Call Get method with no mnemonics (to get all companies)
+	companies, err := client.Companies.Get(nil)
+	if err != nil {
+		log.Fatalf("Error getting companies: %v", err)
+	}
+
+	// Print the results
+	fmt.Printf("Found %d companies\n", len(companies))
+	for _, company := range companies {
+		fmt.Printf("Company: %s (Mnemonic: %s)\n", company.Name, company.Mnemonic)
+	}
+
+	// Test with specific mnemonics
+	mnemonics := []string{"CERN_CWIM"}
+	specificCompanies, err := client.Companies.Get(mnemonics)
+	if err != nil {
+		log.Fatalf("Error getting specific companies: %v", err)
+	}
+
+	fmt.Printf("\nFound %d specific companies\n", len(specificCompanies))
+	for _, company := range specificCompanies {
+		fmt.Printf("Company: %s (Mnemonic: %s)\n", company.Name, company.Mnemonic)
+	}
+}
