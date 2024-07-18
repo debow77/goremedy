190d8527890634526414abbb9e2cb54e45d44531
debow77 <dscottpowell@gmail.com>
2024-07-18 08:34:32 -0500

    Update client.go

--- client.go
+++ client.go
@@ -1,29 +1,44 @@
 package goremedy
 
 import (
-	"gorapid"
+    "gorapid"
 )
+
+// RemedyClientInterface defines the methods for RemedyClient
+type RemedyClientInterface interface {
+    GetRapidClient() *gorapid.RapidClient
+    GetCompanyClientGroup() *CompanyClientGroup
+    // Add other methods that should be part of the interface
+}
 
 // RemedyClient represents the main client for all Remedy transactions
 type RemedyClient struct {
-	// ChangeRequests *CRClientGroup
-	Companies   *CompanyClientGroup
-	rapidClient *gorapid.RapidClient
+    Companies   *CompanyClientGroup
+    rapidClient *gorapid.RapidClient
 }
 
 // NewRemedyClient creates a new RemedyClient instance
 func NewRemedyClient() (*RemedyClient, error) {
-	rapidClient, err := gorapid.NewRapidClient()
-	if err != nil {
-		return nil, err
-	}
+    rapidClient, err := gorapid.NewRapidClient()
+    if (err != nil) {
+        return nil, err
+    }
 
-	client := &RemedyClient{
-		rapidClient: rapidClient,
-	}
+    client := &RemedyClient{
+        rapidClient: rapidClient,
+    }
 
-	// client.ChangeRequests = NewCRClientGroup(client)
-	client.Companies = NewCompanyClientGroup(client)
+    client.Companies = NewCompanyClientGroup(client)
 
-	return client, nil
+    return client, nil
+}
+
+// GetRapidClient returns the RapidClient
+func (rc *RemedyClient) GetRapidClient() *gorapid.RapidClient {
+    return rc.rapidClient
+}
+
+// GetCompanyClientGroup returns the CompanyClientGroup
+func (rc *RemedyClient) GetCompanyClientGroup() *CompanyClientGroup {
+    return rc.Companies
 }
