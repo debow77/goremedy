ce989fc35ebfb82000de52b636091cebbd11f368
dp7168 <dp7168@W2033403.northamerica.cerner.net>
2024-07-18 10:05:22 -0500

    Testing

---
+++ client.go
@@ -0,0 +1,43 @@
+package goremedy
+
+import (
+	"github.cerner.com/OHAIFedAutoSre/gorapid"
+)
+
+// RemedyClientInterface defines the methods for RemedyClient
+type RemedyClientInterface interface {
+	GetRapidClient() *gorapid.RapidClient
+	GetCompanyClientGroup() *CompanyClientGroup
+}
+
+// RemedyClient represents the main client for all Remedy transactions
+type RemedyClient struct {
+	Companies   *CompanyClientGroup
+	rapidClient *gorapid.RapidClient
+}
+
+// NewRemedyClient creates a new RemedyClient instance
+func NewRemedyClient() (*RemedyClient, error) {
+	rapidClient, err := gorapid.NewRapidClient()
+	if err != nil {
+		return nil, err
+	}
+
+	client := &RemedyClient{
+		rapidClient: rapidClient,
+	}
+
+	client.Companies = NewCompanyClientGroup(client)
+
+	return client, nil
+}
+
+// GetRapidClient returns the RapidClient
+func (rc *RemedyClient) GetRapidClient() *gorapid.RapidClient {
+	return rc.rapidClient
+}
+
+// GetCompanyClientGroup returns the CompanyClientGroup
+func (rc *RemedyClient) GetCompanyClientGroup() *CompanyClientGroup {
+	return rc.Companies
+}
